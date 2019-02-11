1ca2a9274bf952796e1f0604b4c5821d6f6a3e6d
yvnat <44072408+yvnat@users.noreply.github.com>
2019-02-11 13:33:32 -0500

    test

---
+++ src/www/Www.java
@@ -0,0 +1,30 @@
+package www;
+
+/**
+ *
+ * @author the entire gang working together
+ */
+public class Www {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
+
+/*
+
+class Transaction {
+    Date date;
+    int customerId;
+}
+
+class Order : Transaction {
+    int number;
+}
+
+class Payment : Transaction {
+    int sum
+}
+
+Transaction[] transactions;
+Order[] upcomingOrders;
+*/
