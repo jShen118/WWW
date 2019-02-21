e86c75d2722f001c04d3e5a573d9f6e77da7de43
yvnat <44072408+yvnat@users.noreply.github.com>
2019-02-21 17:54:29 -0500

    Changes, mainly to Transaction and its children:
    -moved price to Transaction as it is shared by both Order and Payment
    -changed Transaction from containing a Customer to containing a customer ID (small change, for sake of spec adherence)
    -added an abstract report() function to transactions, but DID NOT implement them for child classes
    -fixed customer to correctly have unique IDs
    -changed Order:
     *Each Order now has a unique ID
     *Orders contain a reference to a repairPrice instead of directly storing brand and level
     *slightly changed the various Dates; added promisedDate() which returns the date the order was promised for

--- src/www/Customer.java
+++ src/www/Customer.java
@@ -5,27 +5,30 @@
  */
 package www;
 
+//TODO: complete this class
+
 public class Customer {
-    String firstName;
-    public static int ID = 0;
-    Transaction[] transactions;
-    
-    Customer(String name) {
-        this.firstName = firstName;
-        ID++;
-    }
-    
-    public int debt() {
-        int debt = 0;
-        for(int i = 0; i < transactions.length; i++) {
-            //ERROR HERE: I couldn't figure out how to parse a transaction into Payment or Order
-            if (transactions[i] instanceof Payment) {
-                debt = debt - ((Payment) transactions[i]).amount;
-            }
-            if (transactions[i] instanceof Order) {
-                debt = debt + ((Order) transactions[i]).price;
-            }
-        }
-        return debt;
-    }
+	String firstName;
+	static private int customerNumberDispenser = 0;	//used to give unique customer IDs
+	public int customerNumber;	//the ID of this specific customer
+	Transaction[] transactions;
+	
+	Customer(String name) {
+		this.firstName = firstName;
+		customerNumber = customerNumberDispenser++;	//set customer number to next ID, then increment the dispenser
+	}
+	
+	public int debt() {
+		int debt = 0;
+		for(int i = 0; i < transactions.length; i++) {
+			//ERROR HERE: I couldn't figure out how to parse a transaction into Payment or Order
+			if (transactions[i] instanceof Payment) {
+				debt = debt - ((Payment) transactions[i]).amount;
+			}
+			if (transactions[i] instanceof Order) {
+				debt = debt + ((Order) transactions[i]).amount;
+			}
+		}
+		return debt;
+	}
 }
