7c2dadf846d52d9cd7c22423401b481ef9a2a3f6
yvnat <44072408+yvnat@users.noreply.github.com>
2019-02-21 18:17:04 -0500

    Changes (to Customer):
    -distinguished first/last name as specified by specs
    -added a toString() method (THIS IS NOT A REPORT!)
    -removed the list of transactions and the debt() method
    
    I think it would be best to have customer only store data, and handle the transactions/receivables through a coordinating class that contains both the customers and the transactions. This is for two reasons: The first being that we need to have a class like that anyways, so we might as well put the calculations there. The second reason is that we want to store as little as possible in Customer so we can avoid having to keep updated separate lists of transactions for every customer and instead handle only one main list (KISS). Also, it fits the specs better.
    
    Another thing to note is that, given the current implementation, Transactions have their own report() function while Customer's report will be handled by the coordinating class. My justification for this is that a Transaction's report only requires data stored within the class, while a Customer's report needs to also access data from other classes (for displaying order history, for example). It also gives us more modularity within the Transactions. However, I don't really like this assymmetry, and I think it would be more consistent to have the coordinating class handle all reports.

--- src/www/Customer.java
+++ src/www/Customer.java
@@ -5,30 +5,20 @@
  */
 package www;
 
-//TODO: complete this class
-
 public class Customer {
-	String firstName;
 	static private int customerNumberDispenser = 0;	//used to give unique customer IDs
 	public int customerNumber;	//the ID of this specific customer
-	Transaction[] transactions;
+	public String firstName;
+	public String lastName;
 	
-	Customer(String name) {
+	Customer(String firstName, String lastName) {
 		this.firstName = firstName;
+		this.lastName = lastName;
 		customerNumber = customerNumberDispenser++;	//set customer number to next ID, then increment the dispenser
 	}
 	
-	public int debt() {
-		int debt = 0;
-		for(int i = 0; i < transactions.length; i++) {
-			//ERROR HERE: I couldn't figure out how to parse a transaction into Payment or Order
-			if (transactions[i] instanceof Payment) {
-				debt = debt - ((Payment) transactions[i]).amount;
-			}
-			if (transactions[i] instanceof Order) {
-				debt = debt + ((Order) transactions[i]).amount;
-			}
-		}
-		return debt;
+	public String toString() {
+		return firstName + " " + lastName + "(customer #" + Integer.toString(customerNumber) + ")";
+		//example: "John Smith (customer #193)"
 	}
 }
