95eafcaaefe6753e7adb9efed87b2f14ff39c112
yvnat <44072408+yvnat@users.noreply.github.com>
2019-02-23 19:10:18 -0500

    Beginning implementation of Shop class
    -fixed crash resulting from trying to retrieve prices of nonexistant brand
    -added a private list of transactions to Shop
    -added a private list of customers to Shop
    -added a private PriceTable member to shop
    -added a constructor to Shop
    -moved multilineComment() from Order to Support
    -added bufferCustom() to Support, which buffers with an inputted string
    -implemented help() function in Shop

--- src/www/Shop.java
+++ src/www/Shop.java
@@ -4,12 +4,47 @@
  * and open the template in the editor.
  */
 package www;
-
+import java.util.*;
 /*
  this class contains the data structures abd implements the functions that correspond to the commands
  */
 public class Shop {
-	public void help() {};	//print help
+	private List<Transaction> transactions;
+	private List<Customer> customers;
+	private PriceTable priceTable;
+	
+	Shop() {
+		transactions = new ArrayList<>();
+		customers = new ArrayList<>();
+		priceTable = new PriceTable();
+	}
+	
+	/*------------------commands-------------------------*/
+	public void help() {
+		String message = "├-[help]-----------------------------------------------------------------------------┤\n" +	
+				"　Commands:\n" +
+				"　　help" + Support.bufferCustom("display help\n", 82 - "help".length(), ".") +
+				"　　quit" + Support.bufferCustom("quit the system\n", 82 - "quit".length(), ".") +
+				"　　addrp <brand> <level> <price> <days>" + Support.bufferCustom("add a repair price\n", 82 - "addrp <brand> <level> <price> <days>".length(), ".") +
+				"　　addc <first name> <last name>" + Support.bufferCustom("add a customer\n", 82 - "addc <first name> <last name>".length(), ".") +
+				"　　addo <customer number> <date> <brand> <level> <comment>" + Support.bufferCustom("add an order\n", 82 - "addo <customer number> <date> <brand> <level> <comment>".length(), ".") +
+				"　　addp <customer number> <date> <amount>" + Support.bufferCustom("add a payment\n", 82 - "addp <customer number> <date> <amount>".length(), ".") +
+				"　　comp <order number> <completion date>" + Support.bufferCustom("mark order <order number> as completed\n", 82 - "comp <order number> <completion date>".length(), ".") +
+				"　　printrp" + Support.bufferCustom("print repair price table\n", 82 - "printrp".length(), ".") +
+				"　　printcnum" + Support.bufferCustom("print customers by customer name\n", 82 - "printcnum".length(), ".") +
+				"　　printcname" + Support.bufferCustom("print customers by customer number\n", 82 - "printcname".length(), ".") +
+				"　　printo" + Support.bufferCustom("print orders\n", 82 - "printo".length(), ".") +
+				"　　printp" + Support.bufferCustom("print payments\n", 82 - "printp".length(), ".") +
+				"　　printt" + Support.bufferCustom("print all transactions\n", 82 - "printt".length(), ".") +
+				"　　printr" + Support.bufferCustom("print receivables\n", 82 - "printr".length(), ".") +
+				"　　prints" + Support.bufferCustom("print statements\n", 82 - "prints".length(), ".") +
+				"　　readc <filename>" + Support.bufferCustom("read commands from file with name <filename>\n", 82 - "readc <filename>".length(), ".") +
+				"　　savebs <filename>" + Support.bufferCustom("save bike shop to file <filename>\n", 82 - "savebs <filename>".length(), ".") +
+				"　　restorebs <filename>" + Support.bufferCustom("restore bike shop from save file <filename>\n", 82 - "restorebs <filename>".length(), ".") +
+				"\n　*All dates must be entered in the format of MMDDYYYY (2/5/19 would become 02052019)\n" +
+				"├------------------------------------------------------------------------------------┤";
+		System.out.println(message);
+	};	//print help
 	public void addrp(String brand, RepairLevel level, int price, int days) {};	//add repair price
 	public void addc(String firstName, String lastName) {};	//add customer
 	public void addo(int customerNumber, Date date, String brand, RepairLevel level, String comment) {};	//add order
