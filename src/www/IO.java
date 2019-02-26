536d968f03c3b995488b89a41e88b63cf48b1a30
carlosagr12370 <47610335+carlosagr12370@users.noreply.github.com>
2019-02-25 21:18:03 -0500

    Update IO.java

--- src/www/IO.java
+++ src/www/IO.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package www;
 
-/**
- *
- * @author Melyssa
- */
+package www;
+import java.util.Scanner;
 public class IO {
-    
+    public Shop shop;
+    public String command;
+    protected Scanner s = new Scanner(System.in);
+    public Date currentdate;
+    public void run(){
+        while (!command.equals("quit")){
+            switch(Support.splitStringIntoParts(command)[0]){
+                case("help"): shop.help();
+                break;
+                case("addrp"): shop.addrp(Support.splitStringIntoParts(command)[1], hlevel(Support.splitStringIntoParts(command)[2]),Integer.parseInt(Support.splitStringIntoParts(command)[3]), Integer.parseInt(Support.splitStringIntoParts(command)[4]));
+                break;
+                case("addc"): shop.addc(Support.splitStringIntoParts(command)[1], Support.splitStringIntoParts(command)[2]);
+                break;
+                case("addo"): shop.addo(Integer.parseInt(Support.splitStringIntoParts(command)[1]), Support.splitStringIntoParts(command)[2], Support.splitStringIntoParts(command)[3], Support.splitStringIntoParts(command)[4], Support.splitStringIntoParts(command)[5]);
+                break;
+                case("addp"): shop.addp(Integer.parseInt(Support.splitStringIntoParts(command)[1]), Support.splitStringIntoParts(command)[2],Integer.parseInt(Support.splitStringIntoParts(command)[3]));
+                break;
+                case("comp"):
+                break;
+                case("printrp"): shop.printrp();
+                break;
+                case("printcnum"): shop.printcnum();
+                break;
+                case("printcname"): shop.printcname();
+                break;
+                case("printo"): 
+                break;
+                case("printp"):
+                break;
+                case("printt"):
+                break;
+                case("printr"):
+                break;
+                case("prints"):
+                break;
+                case("readc"):
+                break;
+                case("savebs"):
+                break;
+                case("restorebs"):
+                break;
+            }
+        }
+    }
+    public RepairLevel hlevel(String type){
+        switch(type){
+            case("silver"): return RepairLevel.silver;
+            case("gold"): return RepairLevel.gold;
+            case("platinum"): return RepairLevel.platinum;
+        }
+        return null;
+    }
+    public Date hdate(String date){
+        return null;
+    }
 }
