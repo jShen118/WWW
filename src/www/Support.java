695261bee4af8924123116e071a7c775edbc9553
jShen118 <47192295+jShen118@users.noreply.github.com>
2019-03-12 21:27:34 -0400

    Update Support.java

--- src/www/Support.java
+++ src/www/Support.java
@@ -259,6 +259,64 @@ public class Support {
             return toPrint;
         }
         
+	/*
+	s.addrp("Trek", RepairLevel.silver, 40, 4);
+                s.addrp("Trek", RepairLevel.gold, 60, 7);
+                s.addrp("Trek", RepairLevel.platinum, 100, 10);
+                s.addrp("Cannondale", RepairLevel.silver, 40, 4);
+                s.addrp("Cannondale", RepairLevel.gold, 60, 7);
+                s.addrp("Cannondale", RepairLevel.platinum, 100, 10);
+                s.addrp("Salsa", RepairLevel.silver, 50, 4);
+                s.addrp("Salsa", RepairLevel.gold, 70, 7);
+                s.addrp("Salsa", RepairLevel.platinum, 115, 10);
+                s.addrp("Jamis", RepairLevel.silver, 60, 4);
+                s.addrp("Jamis", RepairLevel.gold, 75, 7);
+                s.addrp("Jamis", RepairLevel.platinum, 120, 10);
+                
+                s.addc("John", "Smith");
+                s.addo(0, new Date(3, 3, 2019), "Trek", RepairLevel.silver, "no comment");
+                s.addo(0, new Date(3, 3, 2019), "Cannondale", RepairLevel.gold, "no comment");
+                s.addo(0, new Date(3, 3, 2019), "Salsa", RepairLevel.platinum, "no comment");
+                s.addc("Susan", "Apples");
+                s.addo(1, new Date(3, 3, 2019), "Jamis", RepairLevel.silver, "no comment");
+                s.addo(1, new Date(3, 3, 2019), "Trek", RepairLevel.gold, "no comment");
+                s.addo(1, new Date(3, 3, 2019), "Cannondale", RepairLevel.platinum, "no comment");
+                s.addc("Jack", "Johnson");
+                s.addo(2, new Date(3, 3, 2019), "Salsa", RepairLevel.silver, "no comment");
+                s.addo(2, new Date(3, 3, 2019), "Jamis", RepairLevel.gold, "no comment");
+                s.addo(2, new Date(3, 3, 2019), "Trek", RepairLevel.platinum, "no comment");
+                s.addc("Jill", "Johnson");
+                s.addo(3, new Date(3, 3, 2019), "Cannondale", RepairLevel.silver, "no comment");
+                s.addo(3, new Date(3, 3, 2019), "Salsa", RepairLevel.gold, "no comment");
+                s.addo(3, new Date(3, 3, 2019), "Jamis", RepairLevel.platinum, "no comment");
+                
+                s.printo();
+                s.printp();
+                s.printr();
+                s.printt();
+                s.printcnum();
+                s.printcname();
+                s.prints();
+                
+                //s.addc("John", "Smith");
+                s.addo(0, new Date(3, 3, 2019), "Trek", RepairLevel.silver, "no comment");
+                s.addo(0, new Date(3, 4, 2019), "Cannondale", RepairLevel.gold, "no comment");
+                s.addo(0, new Date(3, 5, 2019), "Salsa", RepairLevel.platinum, "no comment");
+                //s.addc("Susan", "Apples");
+                s.addo(1, new Date(3, 6, 2019), "Jamis", RepairLevel.silver, "no comment");
+                s.addo(1, new Date(3, 7, 2019), "Trek", RepairLevel.gold, "no comment");
+                s.addo(1, new Date(3, 8, 2019), "Cannondale", RepairLevel.platinum, "no comment");
+                //s.addc("Jack", "Johnson");
+                s.addo(2, new Date(3, 9, 2019), "Salsa", RepairLevel.silver, "no comment");
+                s.addo(2, new Date(3, 8, 2019), "Jamis", RepairLevel.gold, "no comment");
+                s.addo(2, new Date(3, 7, 2019), "Trek", RepairLevel.platinum, "no comment");
+                //s.addc("Jill", "Johnson");
+                s.addo(3, new Date(3, 6, 2019), "Cannondale", RepairLevel.silver, "no comment");
+                s.addo(3, new Date(3, 5, 2019), "Salsa", RepairLevel.gold, "no comment");
+                s.addo(3, new Date(3, 4, 2019), "Jamis", RepairLevel.platinum, "no comment");
+                s.printworr();
+	*/
+	
         static public String monthlyOrderRevenueReport(ArrayList<Order> orders) {
             String toPrint = "├[Monthly Order Revenue Report]-------------------------------------------------------┤";
             //DateRange dateRange = orderDateRange(orders);
